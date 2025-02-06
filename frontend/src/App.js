ec40d25eebd593314678a166f294328696946307
Ankitkumar6398 <ankit.kumar_cs22@gla.ac.in>
2025-02-06 20:12:32 +0530

    create chatting application

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element:<HomePage/>
+    },
+    {
+        path: '/register',
+        element:<Signup/>
+    },
+    {
+        path: '/login',
+        element:<Login/>
+    }
+])
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
