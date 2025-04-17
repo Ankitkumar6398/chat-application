c7336982be7f990e6682daeac53b8bf9e12562b1
Ankitkumar6398 <ankit.kumar_cs22@gla.ac.in>
2025-04-18 02:53:51 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,61 @@
+import Signup from './components/Signup';
 import './App.css';
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import HomePage from "./components/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import io from "socket.io-client";
+import { setSocket } from './redux/socketSlice';
+import { setOnlineUsers } from './redux/userSlice';
 
+// Define BASE_URL directly instead of importing it
+const BASE_URL = "http://localhost:8080";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element:<HomePage/>
-    },
-    {
-        path: '/register',
-        element:<Signup/>
-    },
-    {
-        path: '/login',
-        element:<Login/>
-    }
-])
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+]);
+
 function App() {
+  const { authUser } = useSelector(store => store.user);
+  const { socket } = useSelector(store => store.socket);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io(`${BASE_URL}`, {
+        query: {
+          userId: authUser._id
+        }
+      });
+      dispatch(setSocket(socketio));
+
+      socketio?.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+      return () => socketio.close();
+    } else {
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
+      }
+    }
+  }, [authUser]);
+
   return (
-    <div className="App">
-      <RouterProvider router={router}/>
+    <div className="app-container">
+      <RouterProvider router={router} />
     </div>
   );
 }
